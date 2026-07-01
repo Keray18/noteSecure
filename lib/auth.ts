ca6b2dba67aeb731b36b5e6bdfd25a2a1f9b1518
Keray18 <keshavbahoray18@gmail.com>
2026-07-01 23:05:54 +0530

    fixed typescript validation for jwt

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,9 @@ import jwt from "jsonwebtoken"
 
 const jwt_secret = process.env.JWT_SECRET
 
+if (!jwt_secret) {
+  throw new Error("JWT_SECRET is not defined");
+}
 
 export const generateToken = (userId: string) => {
     return jwt.sign(
