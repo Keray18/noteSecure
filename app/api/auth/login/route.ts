e76663d9eba12cba61a1759565c7fa6df64fffde
Keray18 <keshavbahoray18@gmail.com>
2026-07-01 09:53:48 +0530

    login route complete

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,100 @@
+import { prisma } from "@/lib/prisma"
+import { generateToken } from "@/lib/auth"
+import { loginSchema } from "@/validation/auth"
+import bcrypt from "bcrypt"
+import { NextResponse } from "next/server"
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json()
+    const result = loginSchema.safeParse(body)
+
+    if (!result.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: result.error.issues[0].message,
+        },
+        { status: 400 }
+      );
+    }
+
+    const { email, password } = result.data;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid email or password",
+        },
+        { status: 401 }
+      )
+    }
+
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid email or password",
+        },
+        { status: 401 }
+      );
+    }
+
+    // Step 5: Generate JWT
+    const token = generateToken(user.id);
+
+    // Step 6: Create response
+    const response = NextResponse.json(
+      {
+        success: true,
+        message: "Login successful",
+        data: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+
+    // Step 7: Store JWT in cookie
+    response.cookies.set({
+      name: "token",
+      value: token,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+      path: "/",
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error)
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    )
+  }
+}
--- validation/auth.ts
+++ validation/auth.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const registerSchema = z.object({
   name: z.string().min(2, "Name must contain at least 2 characters"),
@@ -6,4 +6,9 @@ export const registerSchema = z.object({
   email: z.email("Invalid email address"),
 
   password: z.string().min(6, "Password must contain at least 8 characters"),
-});
+})
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(1, "Password is required"),
+})
